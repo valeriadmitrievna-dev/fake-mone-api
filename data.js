5614233a64a711588760270243511d0ef0641856
Valeria Dmitrievna <dev.valeriadmitrievna@gmail.com>
2023-01-17 09:58:41 +0300

    simple cards

---
+++ data.js
@@ -0,0 +1,58 @@
+module.exports = {
+  cards: [
+    {
+      date: new Date("2023-01-15T08:00:00"),
+      service: {
+        duration: 15,
+      },
+    },
+    {
+      date: new Date("2023-01-15T08:15:00"),
+      service: {
+        duration: 15,
+      },
+    },
+    {
+      date: new Date("2023-01-15T08:30:00"),
+      service: {
+        duration: 30,
+      },
+    },
+    {
+      date: new Date("2023-01-15T09:15:00"),
+      service: {
+        duration: 90,
+      },
+    },
+    {
+      date: new Date("2023-01-16T09:00:00"),
+      service: {
+        duration: 45,
+      },
+    },
+    {
+      date: new Date("2023-01-17T10:45:00"),
+      service: {
+        duration: 15,
+      },
+    },
+    {
+      date: new Date("2023-01-17T11:30:00"),
+      service: {
+        duration: 15,
+      },
+    },
+    {
+      date: new Date("2023-01-18T11:00:00"),
+      service: {
+        duration: 45,
+      },
+    },
+    {
+      date: new Date("2023-01-18T13:00:00"),
+      service: {
+        duration: 105,
+      },
+    },
+  ],
+};
--- generate.js
+++ generate.js
@@ -1,79 +1,104 @@
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
+const _ = require("lodash");
+const { faker } = require("@faker-js/faker");
+faker.setLocale("ru");
+faker.locale = "ru";
+const fns = require("date-fns");
 
-module.exports = function () {
-  const _ = require("lodash");
-  const { faker } = require("@faker-js/faker");
-  faker.setLocale("ru");
-  faker.locale = "ru";
+const data = require("./data");
 
-  const master_purposes = [
-    "Парикмахер-стилист",
-    "Мастер маникюра и педикюра",
-    "Косметолог",
-    "Визажист",
-    "Массажист",
-    "Мастер-универсал",
-  ];
+const durations = [15, 30, 45, 60, 90, 120, 150, 180];
+const purposes = [
+  "Парикмахер-стилист",
+  "Мастер маникюра и педикюра",
+  "Косметолог",
+  "Визажист",
+  "Массажист",
+  "Мастер-универсал",
+];
 
-  const durations = [15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180];
+const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const randomFromArray = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+const toSameDate = (toDate, date) =>
+  fns.setYear(
+    fns.setMonth(fns.setDate(toDate, fns.getDate(date)), fns.getMonth(date)),
+    fns.getYear(date)
+  );
+
+const clients = _.times(100, (n) => {
+  const cardNumbers = faker.random.numeric(4);
+  const cardAlphas = faker.random.alpha(2).toUpperCase();
+  const card = cardNumbers + "/" + cardAlphas;
 
   return {
-    clients: _.times(100, (n) => {
-      const cardNumbers = faker.random.numeric(4);
-      const cardAlphas = faker.random.alpha(2).toUpperCase();
-      const card = cardNumbers + "/" + cardAlphas;
+    id: faker.datatype.uuid(),
+    avatar: faker.image.avatar(),
+    age: faker.random.numeric(2),
+    registeredAt: faker.date.past(),
+    phone: faker.phone.number("+# (###) ###-##-##"),
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    sex: faker.helpers.arrayElement(["М", "Ж"]),
+    card,
+  };
+});
 
-      return {
-        id: faker.datatype.uuid(),
-        avatar: faker.image.avatar(),
-        age: faker.random.numeric(2),
-        registeredAt: faker.date.past(),
-        phone: faker.phone.number("+# (###) ###-##-##"),
-        firstname: faker.name.firstName(),
-        lastname: faker.name.lastName(),
-        sex: faker.helpers.arrayElement(["М", "Ж"]),
-        card,
-      };
-    }),
-    masters: _.times(20, (n) => {
-      return {
-        id: faker.datatype.uuid(),
-        firstname: faker.name.firstName(),
-        lastname: faker.name.lastName(),
-        avatar: faker.image.avatar(),
-        purpose: faker.helpers.arrayElement(master_purposes),
-        phrase: faker.lorem.words(randomNumber(2, 7)),
-        about: _.times(randomNumber(2, 5), () =>
-          faker.lorem.words(randomNumber(4, 16))
-        ),
-        bests: _.times(randomNumber(0, 5), () =>
-          faker.lorem.words(randomNumber(4, 16))
-        ),
-        reviews: _.times(randomNumber(0, 5), () => faker.lorem.paragraph()),
-      };
-    }),
-    services: _.times(20, () => {
-      const codeNumbers = faker.random.numeric(4);
-      const codeAlphas = faker.random.alpha(2).toUpperCase();
-      const code = codeNumbers + "/" + codeAlphas;
+const masters = _.times(40, (n) => {
+  return {
+    id: faker.datatype.uuid(),
+    firstname: faker.name.firstName(),
+    lastname: faker.name.lastName(),
+    avatar: faker.image.avatar(),
+    purpose: faker.helpers.arrayElement(purposes),
+    phrase: faker.lorem.words(random(2, 7)),
+    about: _.times(random(2, 5), () => faker.lorem.words(random(4, 16))),
+    bests: _.times(random(0, 5), () => faker.lorem.words(random(4, 16))),
+    reviews: _.times(random(0, 5), () => faker.lorem.paragraph()),
+  };
+});
 
-      return {
-        id: faker.datatype.uuid(),
-        title: faker.lorem.words(randomNumber(1, 3)),
-        duration: faker.helpers.arrayElement(durations),
-        code,
-        about: _.times(randomNumber(2, 5), () =>
-          faker.lorem.words(randomNumber(4, 16))
-        ),
-        steps: _.times(randomNumber(3, 5), () =>
-          faker.lorem.words(randomNumber(4, 16))
-        ),
-        price: faker.commerce.price(300, 20000, 0),
-        reviews: _.times(randomNumber(0, 5), () => faker.lorem.paragraph()),
-        photos: _.times(randomNumber(0, 5), () => faker.image.fashion())
-      };
-    }),
+const services = _.times(40, () => {
+  const codeNumbers = faker.random.numeric(4);
+  const codeAlphas = faker.random.alpha(2).toUpperCase();
+  const code = codeNumbers + "/" + codeAlphas;
+
+  return {
+    id: faker.datatype.uuid(),
+    title: faker.lorem.words(random(1, 3)),
+    duration: faker.helpers.arrayElement(durations),
+    code,
+    about: _.times(random(2, 5), () => faker.lorem.words(random(4, 16))),
+    steps: _.times(random(3, 5), () => faker.lorem.words(random(4, 16))),
+    price: faker.commerce.price(300, 20000, 0),
+    reviews: _.times(random(0, 5), () => faker.lorem.paragraph()),
+    photos: _.times(random(0, 5), () => faker.image.fashion()),
+  };
+});
+
+const cards = data.cards.map((card) => {
+  const diff = fns.differenceInDays(card.date, data.cards[0].date);
+  const client = randomFromArray(clients);
+  const master = randomFromArray(masters);
+  const services_15 = services.filter((s) => s.duration === 15);
+  const fit_services = services.filter(
+    (s) => s.duration === card.service.duration
+  );
+  const service = randomFromArray(fit_services) || randomFromArray(services_15);
+
+  return {
+    ...card,
+    date: fns.addDays(toSameDate(card.date, new Date()), diff),
+    client,
+    master,
+    service,
+  };
+});
+
+module.exports = function () {
+  return {
+    clients,
+    masters,
+    services,
+    cards
   };
 };
